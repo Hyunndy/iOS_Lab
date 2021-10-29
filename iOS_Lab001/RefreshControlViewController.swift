e7664c56edaff87edec9c50dea2bb308da302823
Hyunndy <yoosa3004@deali.net>
2021-10-30 01:33:28 +0900

    - refreshControl 제거 - View 형태로 변환 - Image 를 가리기 위해 배민처럼 스택뷰 위에 밑으색 뷰를 추가

--- iOS_Lab001/RefreshControlViewController.swift
+++ iOS_Lab001/RefreshControlViewController.swift
@@ -12,43 +12,79 @@ import RxSwift
 import RxCocoa
 
 var colorData: [UIColor] = [.red, .orange, .yellow, .green, .blue, .magenta, .purple]
+var imageData: [String] = ["img_orderlist_new_20", "img_orderlist_package_20", "img_orderlist_request_20",
+    "img_orderlist_check_20"]
 
 class RefreshControlViewController: CMViewController {
     
     var rxColorDataRelay = BehaviorRelay<[UIColor]>.init(value: colorData)
     
-    let scvIvContainer = UIScrollView()
-    let stvIvContainer = UIStackView()
+    let ivRefresh = UIImageView()
     let vRefresh = UIView()
-    let refreshControl = UIRefreshControl()
     
     let scvContainer = UIScrollView()
     let stvContainer = UIStackView()
     
+    let mint: UIColor = UIColor(red: 0/255, green: 201/255, blue: 161/255, alpha: 1.0)
+    
     override func loadView() {
         super.loadView()
         
+        
+        self.navigationController?.navigationBar.barTintColor = mint
+        
+        
+        self.vContent.addSubview(vRefresh)
+        vRefresh.then {
+            $0.backgroundColor = mint
+        }.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+            $0.height.equalTo(0.0)
+        }
+        
         self.vContent.addSubview(scvContainer)
-        scvContainer.then {
+        scvContainer.then { [unowned self] in
             $0.delegate = self
             $0.alwaysBounceVertical = true
         }.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
         
+        let vTop = UIView()
+        self.scvContainer.addSubview(vTop)
+        vTop.then {
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 15
+            $0.backgroundColor = mint
+            $0.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        }.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+            $0.height.equalTo(20.0)
+        }
+        
         self.scvContainer.addSubview(stvContainer)
         stvContainer.then {
+            $0.backgroundColor = .white
             $0.axis = .vertical
             $0.alignment = .center
-            $0.distribution = .fillEqually
+            $0.distribution = .fill
             $0.spacing = 10.0
         }.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(10.0)
+            $0.top.equalTo(vTop.snp.bottom)
             $0.left.right.equalToSuperview()
             $0.bottom.equalToSuperview().offset(-10.0)
             $0.width.equalTo(UIScreen.main.bounds.size.width)
         }
-
+        
+        let vPadding = UIView()
+        stvContainer.addArrangedSubview(vPadding)
+        vPadding.then {
+            $0.backgroundColor = .white
+        }.snp.makeConstraints {
+            $0.left.right.equalToSuperview()
+            $0.height.equalTo(10.0)
+        }
+        
         for color in colorData {
             let vColor = UIView()
             self.stvContainer.addArrangedSubview(vColor)
@@ -61,114 +97,27 @@ class RefreshControlViewController: CMViewController {
                 $0.centerX.equalToSuperview()
             }
         }
-//
-        self.refreshControl.addSubview(vRefresh)
-        vRefresh.snp.makeConstraints {
-            $0.centerX.equalTo(self.refreshControl.frame.width/2)
-            $0.centerY.equalTo(0.0)
-            $0.height.equalTo(50.0)
-        }
-
-
-        self.vRefresh.addSubview(scvIvContainer)
-        scvIvContainer.then {
-            $0.alwaysBounceVertical = true
-            $0.contentSize = CGSize(width: 50.0, height: 50.0)
-            $0.isUserInteractionEnabled = false
-        }.snp.makeConstraints {
-            $0.top.bottom.left.equalToSuperview()
-            $0.width.equalTo(50.0)
-        }
-
-        let stvIvContainer = UIStackView()
-        scvIvContainer.addSubview(stvIvContainer)
-        stvIvContainer.then {
-            $0.axis = .vertical
-            $0.spacing = 0.0
-            $0.distribution = .fillEqually
-        }.snp.makeConstraints {
-            $0.top.equalToSuperview()//.offset(25.0)
-            $0.left.right.bottom.equalToSuperview()
-        }
+        //
         
-        var padding = UIView()
-        stvIvContainer.addArrangedSubview(padding)
-        _ = padding.snp.makeConstraints {
-//            $0.top.bottom.left.equalToSuperview()
-            $0.left.right.equalToSuperview()
-            $0.size.equalTo(CGSize(width: 50.0, height: 50.0))
-        }
-
-        var ivRefresh = UIImageView()
-        stvIvContainer.addArrangedSubview(ivRefresh)
-        ivRefresh.then {
-            $0.image = UIImage(named: "img_orderlist_check_20")
-            $0.contentMode = .scaleToFill
-        }.snp.makeConstraints {
-//            $0.top.bottom.left.equalToSuperview()
-            $0.left.right.equalToSuperview()
-            $0.size.equalTo(CGSize(width: 50.0, height: 50.0))
-        }
-
-        var ivRefresh2 = UIImageView()
-        stvIvContainer.addArrangedSubview(ivRefresh2)
-        ivRefresh2.then {
-            $0.image = UIImage(named: "img_orderlist_new_20")
-            $0.contentMode = .scaleToFill
-        }.snp.makeConstraints {
-//            $0.top.bottom.left.equalToSuperview()
-            $0.left.right.equalToSuperview()
-            $0.size.equalTo(CGSize(width: 50.0, height: 50.0))
-        }
-
-        var ivRefresh3 = UIImageView()
-        stvIvContainer.addArrangedSubview(ivRefresh3)
-        ivRefresh3.then {
-            $0.image = UIImage(named: "img_orderlist_package_20")
-            $0.contentMode = .scaleToFill
-        }.snp.makeConstraints {
-//            $0.top.bottom.left.equalToSuperview()
-            $0.left.right.equalToSuperview()
-            $0.size.equalTo(CGSize(width: 50.0, height: 50.0))
-        }
-
         let lbRefreshTitle = UILabel()
         vRefresh.addSubview(lbRefreshTitle)
         lbRefreshTitle.then {
             $0.text = "땡겨욧"
             $0.font = .systemFont(ofSize: 15.0, weight: .bold)
         }.snp.makeConstraints {
-            $0.top.bottom.right.equalToSuperview()
-            $0.left.equalTo(self.scvIvContainer.snp.right).offset(10.0)
+            $0.top.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview()
+//            $0.left.equalTo(self.scvIvContainer.snp.right).offset(10.0)
         }
 
-        self.scvContainer.refreshControl = self.refreshControl
-        _ = self.scvContainer.refreshControl?.then {
-            $0.tintColor = .clear
-            $0.addTarget(self, action: #selector(pullToRefresh), for: .valueChanged)
+        vRefresh.addSubview(ivRefresh)
+        ivRefresh.then {
+            $0.image = UIImage(named: "img_orderlist_new_20")
+            $0.contentMode = .center
+        }.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.right.equalTo(lbRefreshTitle.snp.left).offset(-10.0)
         }
-//        /*
-//         img_orderlist_check_20
-//         img_orderlist_new_20
-//         */
-
-    }
-    
-    @objc func pullToRefresh() {
-        
-        // 화면 당김이 임계점을 넘으면 자동으로 beginRefreshing() 메서드 호출
-//        let colors = self.rxColorDataRelay.value
-////
-//        self.rxColorDataRelay.accept(colors.reversed())
-        self.scvContainer.refreshControl?.beginRefreshing()
-        
-        
-        
-        // 새로고침이 완료되면 명시적으로 endRefresh() 호출
-        DispatchQueue.main.async {
-           self.scvContainer.refreshControl?.endRefreshing()
-        }
-        
     }
     
     override func viewDidLoad() {
@@ -178,12 +127,9 @@ class RefreshControlViewController: CMViewController {
             guard let s = self else { return }
             
             for (idx, subView) in s.stvContainer.arrangedSubviews.enumerated() {
-                subView.backgroundColor = colors[idx]
+                if idx == 0 { continue }
+                subView.backgroundColor = colors[idx-1]
             }
-                    DispatchQueue.main.async { [weak self] in
-                        guard let s = self else { return }
-                        s.refreshControl.endRefreshing()
-                    }
             
         }).disposed(by: self.disposeBag)
     }
@@ -192,34 +138,50 @@ class RefreshControlViewController: CMViewController {
 extension RefreshControlViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
-        let frame = self.refreshControl.frame.origin.y
-        let distance = max(0.0, -(frame))
-
-//        print("######## \(distance)")
-
-        self.vRefresh.snp.updateConstraints {
-            $0.centerX.equalTo(self.refreshControl.frame.width/2)
-            $0.centerY.equalTo(distance/2)
-        }
-
-//        print("######## RefreshControl FrameSize = \(self.refreshControl.frame.size)")
-//        print("######## scrollView = \(scrollView.contentOffset)")
+        let frame = self.vRefresh.frame.maxY
+        //        let distance = max(0.0, -(frame))
+        
+        let distance = scrollView.frame.origin.y - self.vRefresh.frame.origin.y
+        print("######## distance = \(distance)")
+        
         let offset = -scrollView.contentOffset.y
-        let paging = floor(offset/50.0)
-        print("######## paging = \(scrollView.contentOffset)")
+        self.vRefresh.snp.updateConstraints {
+            $0.top.left.right.equalToSuperview()
+            $0.height.equalTo(offset)
+        }
+        
+            let paging = floor(offset/40.0)
+            print("######## paging = \(scrollView.contentOffset)")
 
-        // offset이 50.0 * 서브뷰갯수가 넘어간다면,
-        // 200 / 50.0 = 4
-        // 250 / 50.0 = 4
+            // offset이 50.0 * 서브뷰갯수가 넘어간다면,
+            // 200 / 50.0 = 4
+            // 250 / 50.0 = 4
 
-        // ((200 / 50.0) % 3)
-        let padding = floor((frame/50.0).truncatingRemainder(dividingBy: 3.0))
+            // ((200 / 50.0) % 3)
+            let padding = Int(floor((frame/40.0).truncatingRemainder(dividingBy: 4.0)))
 
-        print("######## padding = \(padding)")
-        self.scvIvContainer.contentOffset.y = -(padding * 50.0)
+            print("######## padding = \(padding)")
 
+        if UIImage(named: imageData[padding]) != self.ivRefresh.image {
+            self.ivRefresh.setImage(UIImage(named: imageData[padding]), animated: true)
+        }
+        
+//        self.ivRefresh.image = UIImage(named: imageData[padding])
+        print("imageData = \(imageData[padding])")
 
+    }
+}
 
-        print("######## scvInvContainer = \(self.scvIvContainer.contentOffset)")
+extension UIImageView{
+    func setImage(_ image: UIImage?, animated: Bool = true) {
+        let duration = animated ? 0.1 : 0.0
+        
+        var frame = self.frame
+        frame.origin.y -= 30
+        self.frame = frame
+        UIView.transition(with: self, duration: duration, options: .curveLinear, animations: {
+            self.frame.origin.y += 30
+            self.image = image
+        }, completion: nil)
     }
 }
