aafdce217442f6099719552182c0b44bfc05bb22
Hyunndy <yoosa3004@naver.com>
2021-12-02 23:55:11 +0900

    이제그만놔줘요 적용 refreshing 끝나기 전에 이모지 변경 안되게 적용

--- iOS_Lab001/RefreshControlViewController.swift
+++ iOS_Lab001/RefreshControlViewController.swift
@@ -14,31 +14,81 @@ import RxCocoa
 var colorData: [UIColor] = [.red, .orange, .yellow, .green, .blue, .magenta, .purple]
 var imageData: [UIImage?] = [UIImage(named: "01"), UIImage(named: "02"), UIImage(named: "03"), UIImage(named: "04"), UIImage(named: "05"), UIImage(named: "06"), UIImage(named: "07"), UIImage(named: "08"), UIImage(named: "09")]
 
+class AmazingRefreshControl: UIView {
+    
+    let ivRefresh = UIImageView()
+    let lbRefreshTitle = UILabel()
+    let lbLetMeGo = UILabel()
+    
+    // 새로고침중인지
+    var isRefreshing: Bool = false
+    
+    init() {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .systemRed
+        
+        self.addSubview(lbRefreshTitle)
+        lbRefreshTitle.then {
+            $0.text = "땡겨욧"
+            $0.font = .systemFont(ofSize: 20.0, weight: .bold)
+        }.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview().offset(10.0)
+        }
+        
+        self.addSubview(ivRefresh)
+        ivRefresh.then {
+            $0.contentMode = .center
+        }.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.right.equalTo(lbRefreshTitle.snp.left).offset(-10.0)
+        }
+        
+        let vTest = UIView()
+        self.addSubview(vTest)
+        vTest.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(200.0)
+            $0.left.right.bottom.equalToSuperview()
+        }
+        
+        vTest.addSubview(lbLetMeGo)
+        lbLetMeGo.then {
+            $0.text = "이제 그만 놔줘요..."
+            $0.font = .systemFont(ofSize: 10.0, weight: .bold)
+        }.snp.makeConstraints {
+            $0.centerX.bottom.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 class RefreshControlViewController: CMViewController {
     
     var rxColorDataRelay = BehaviorRelay<[UIColor]>.init(value: colorData)
     
-    let ivRefresh = UIImageView()
-    let vRefresh = UIView()
+    
+    let vRefresh = AmazingRefreshControl()
     
     let scvContainer = UIScrollView()
     let stvContainer = UIStackView()
     
     var flag = false
-    var imageAnimation = false
+    var isDeceleratingAnimating = false
     
     let mint: UIColor = UIColor(red: 0/255, green: 201/255, blue: 161/255, alpha: 1.0)
     
     override func loadView() {
         super.loadView()
         
-        
         self.navigationController?.navigationBar.barTintColor = mint
-        
         
         self.vContent.addSubview(vRefresh)
         vRefresh.then {
-            $0.backgroundColor = mint
+            $0.backgroundColor = .systemRed
         }.snp.makeConstraints {
             $0.top.left.right.equalToSuperview()
             $0.height.equalTo(0.0)
@@ -48,7 +98,7 @@ class RefreshControlViewController: CMViewController {
         scvContainer.then { [unowned self] in
             $0.delegate = self
             $0.alwaysBounceVertical = true
-            $0.decelerationRate = .fast
+            $0.isPagingEnabled = false
         }.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
@@ -100,27 +150,8 @@ class RefreshControlViewController: CMViewController {
                 $0.centerX.equalToSuperview()
             }
         }
-        //
         
-        let lbRefreshTitle = UILabel()
-        vRefresh.addSubview(lbRefreshTitle)
-        lbRefreshTitle.then {
-            $0.text = "땡겨욧"
-            $0.font = .systemFont(ofSize: 20.0, weight: .bold)
-        }.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.centerX.equalToSuperview().offset(10.0)
-//            $0.left.equalTo(self.scvIvContainer.snp.right).offset(10.0)
-        }
 
-        vRefresh.addSubview(ivRefresh)
-        ivRefresh.then {
-            $0.image = UIImage(named: "img_review_thumbsdn_s_60")
-            $0.contentMode = .center
-        }.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.right.equalTo(lbRefreshTitle.snp.left).offset(-10.0)
-        }
     }
     
     override func viewDidLoad() {
@@ -142,111 +173,91 @@ extension RefreshControlViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
         let frame = self.vRefresh.frame.maxY
-        //        let distance = max(0.0, -(frame))
-        
-        let distance = scrollView.frame.origin.y - self.vRefresh.frame.origin.y
-//        print("######## distance = \(distance)")
-        
         let offset = -scrollView.contentOffset.y
         
-        if imageAnimation == false {
-            self.vRefresh.snp.updateConstraints {
-                $0.top.left.right.equalToSuperview()
-                $0.height.equalTo(offset)
+        if isDeceleratingAnimating == false {
+    
+                // 높이 조절
+                self.vRefresh.snp.updateConstraints {
+                    $0.top.left.right.equalToSuperview()
+                    $0.height.equalTo(offset)
+                }
+            
+            print("#### Hyunndy Test 높이 = \(offset)")
+            if offset > 200.0 {
+                
             }
             
-            print("######################## 높이 값 바꾼다!!")
         }
         
+        let paging = Int(floor((frame/40.0).truncatingRemainder(dividingBy: CGFloat(imageData.count))))
         
-        
-            let paging = floor(offset/40.0)
-            //print("######## contentOffset = \(scrollView.contentOffset)")
-
-            // offset이 50.0 * 서브뷰갯수가 넘어간다면,
-            // 200 / 50.0 = 4
-            // 250 / 50.0 = 4
-
-            // ((200 / 50.0) % 3)
-            let padding = Int(floor((frame/40.0).truncatingRemainder(dividingBy: 9.0)))
-
-//            print("######## padding = \(padding)")
-
-        if imageAnimation == false && flag == false {
+        if self.vRefresh.isRefreshing == false {
             
-            if imageData[padding] != self.ivRefresh.image {
-                self.ivRefresh.setImage(imageData[padding], animated: true)
+            if imageData[paging] != self.vRefresh.ivRefresh.image {
+                self.vRefresh.ivRefresh.setImage(imageData[paging], animated: true)
             }
         }
         
-//        self.ivRefresh.image = UIImage(named: imageData[padding])
-//        print("imageData = \(imageData[padding])")
-
-        
-        if flag == true && scrollView.contentOffset.y >= -70.0 && scrollView.contentOffset.y <= -60.0 {
-            
+        // 스크롤 놨을 때 느리게 움직이고 애니메이션 진행되는 부분
+        if scrollView.isDecelerating == true && scrollView.contentOffset.y >= -70.0 && scrollView.contentOffset.y <= -60.0 {
             
             scrollView.setContentOffset(CGPoint(x: 0.0, y: -60.0), animated: true)
             
-            if self.imageAnimation == false {
+            if self.isDeceleratingAnimating == false {
+                
+                scrollView.isUserInteractionEnabled = false
                 
                 
                 DispatchQueue.main.async {
-                    self.ivRefresh.stopAnimating()
-                    self.ivRefresh.frame.origin.y = 40.0
-
+                    self.vRefresh.ivRefresh.stopAnimating()
+                    self.vRefresh.ivRefresh.frame.origin.y = 40.0
+                    
                 }
                 
-                self.ivRefresh.layoutIfNeeded()
+//                self.ivRefresh.layoutIfNeeded()
                 
-                self.imageAnimation = true
-                self.ivRefresh.transition(duration: 0.05, targetImage: imageData[0], completion: {
-                    self.imageAnimation = false
+                self.vRefresh.isRefreshing = true
+                self.isDeceleratingAnimating = true
+                self.vRefresh.ivRefresh.transition(duration: 0.05, targetImage: imageData[0], completion: {
+                    self.isDeceleratingAnimating = false
                     scrollView.setContentOffset(.zero, animated: true)
                 })
-                
             }
-            
         }
         
-        if scrollView.contentOffset == .zero {
-            self.imageAnimation = false
-        }
+        
+//
+//        if scrollView.contentOffset == .zero {
+//            self.isAnimating = false
+//        }
     }
     
     // 끝나기 직전
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-
-        //print(#function)
-        //print("velocity ===== \(velocity)")
-        
-        
-        
-        //        print("#####으아아악 = \(scrollView.contentOffset)")
-//        print("#####으아아악2222 = \(targetContentOffset.pointee)")
     }
     
     func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
         print(#function)
+        if self.isDeceleratingAnimating == false && scrollView.contentOffset == .zero {
+            scrollView.isUserInteractionEnabled = true
+            self.vRefresh.isRefreshing = false
+        }
     }
     
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        //print(#function)
-        flag = true
+//        flag = true
         imageData.shuffle()
     }
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        //print(#function)
-        flag = false
+//        flag = false
     }
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        //print(#function)
     }
     
     func scrollViewDidChangeAdjustedContentInset(_ scrollView: UIScrollView) {
-        //print(#function)
     }
 }
 
@@ -265,31 +276,26 @@ extension UIImageView{
     
     func transition(duration:CGFloat, targetImage: UIImage?, completion: @escaping () -> Void) {
         
-        print("@@@@@@ duration = \(duration) @@@@@@@")
-        print("@@@@@@ 시작할 때 origin 위치 = \(self.frame.origin.y) @@@@@@@")
         var idx = (imageData.firstIndex(of: targetImage) ?? 0) + 1
         self.setImage2(duration: duration, targetImage, animated: true, success: {
-                
+            
             if targetImage == imageData[5] {
-                    completion()
-                    print("completion#######")
-                    //print("")
-                } else {
-                    print("idx = \(idx)#######")
-                    self.transition(duration: duration + 0.1, targetImage: imageData[idx], completion: completion)
-                }
-            })
+                completion()
+            } else {
+                self.transition(duration: duration + 0.1, targetImage: imageData[idx], completion: completion)
+            }
+        })
         
     }
     
     func setImage2(duration: CGFloat, _ image: UIImage?, animated: Bool = true, success: @escaping () -> Void) {
-
+        
         
         var frame = self.frame
         frame.origin.y = 40
         self.frame = frame
         self.image = image
-        self.layoutIfNeeded()
+//        self.layoutIfNeeded()
         UIView.transition(with: self, duration: duration, options: .curveLinear, animations: {
             if image == imageData[5] {
                 self.frame.origin.y = -10
@@ -298,12 +304,11 @@ extension UIImageView{
             }
             
         }, completion: { voo in
-            print("끝날 때 origin 위치 !!!!!!!!!!!!!!!1 \(self.frame.origin.y)")
             if voo == true {
                 
                 if image != imageData[5] {
                     self.frame.origin.y = 40.0
-                    self.layoutIfNeeded()
+//                    self.layoutIfNeeded()
                     success()
                 } else {
                     UIView.transition(with: self, duration: 0.3, options: .curveLinear, animations: {
@@ -315,9 +320,6 @@ extension UIImageView{
                     })
                     
                 }
-                
-                
-                
             }
         })
     }
